7097b634388043e2bff7b226c921b135e82ff9d9
Stephen Carlin <scarlinj@gmail.com>
2022-06-14 21:20:19 -0700

    added notes to structure js file

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Var set for Today's date and time using Moment.js
+
+//Displays current date and time
+
+  //Add and Remove color coding for past, present, and future events
+
+    //Recall items from local storage
+
+      //call app
